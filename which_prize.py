a9b0a5c4fe258e5124a6c9c4680bd6b4fb9d6ba9
RosalesG <gyrosales1@gmail.com>
2018-03-17 22:16:13 -0400

    worked on lesson 2 items from udacity

--- which_prize.py
+++ which_prize.py
@@ -13,3 +13,25 @@ def which_prize(points):
 	
 	
 print(which_prize(15))
+
+
+
+
+def which_prize2(points):
+    """
+    Returns the number of prize-winning message, given a number of points
+    """
+    prize = None
+    if points <= 50:
+        prize = "a wooden rabbit"
+    elif 151 <= points <= 180:
+        prize = "a wafer-thin mint"
+    elif points >= 181:
+        prize = "a penguin"
+
+    if prize:
+        return "Congratulations! You have won " + prize + "!"
+    else:
+        return "Oh dear, no prize this time."
+		
+print (which_prize2(15))
